7d804b3d2e8ae4bcfe3635599a71ed6d519cbf55
rishik <rishikrc3@gmail.com>
2025-09-11 17:53:30 +0100

    Interface Segregaton

---
+++ SOLID/InterfaceSegregation/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+interface IBad
+{
+    public void Work();
+    public void Eat();
+}
+
+interface IWork
+{
+    public void Work();
+}
+
+interface IEat
+{
+    public void Eat();
+}
+
+public class Robot: IWork
+{
+    public void Work()
+    {
+        Console.WriteLine("Robot work");
+    }
+}
+
+class Prog
+{
+    public static void Main(string[] args)
+    {
+        Robot robot = new Robot();
+        robot.Work();
+    }
+}
