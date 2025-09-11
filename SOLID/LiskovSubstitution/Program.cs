a39ec0d01bbcd8b6b6245d8259ac7e1e828c65d6
rishik <rishikrc3@gmail.com>
2025-09-11 14:09:20 +0100

    LSP

---
+++ SOLID/LiskovSubstitution/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+public class Bird
+{
+    public virtual void Fly()
+    {
+        Console.WriteLine("Bird is Flying");
+    }
+}
+
+public class Eagle: Bird
+{
+    public override void Fly()
+    {
+        Console.WriteLine("Eagle is Flying");
+    }
+}
+
+public class Penguin: Bird
+{
+    public override void Fly()
+    {
+        throw new Exception("Penguins can't fly");
+    }
+}
+public class BirdTrainer
+{
+    public static void TrainBird(Bird bird)
+    {
+        Console.WriteLine("Training bird to fly...");
+        bird.Fly(); 
+    }
+}
+
+
+public class Prog
+{
+    public static void Main(string[] args)
+    {
+        Bird eagle = new Eagle();
+        
+        BirdTrainer.TrainBird(eagle);
+    }
+}
